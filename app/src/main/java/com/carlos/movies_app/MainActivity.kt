c03adda9a80811eaa020fa2c78b3a399ef12cd8c
Carlos Alves <carlosaalves07@outlook.com>
2023-12-17 17:14:08 -0300

    feat: initial commit

---
+++ app/src/main/java/com/carlos/movies_app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.carlos.movies_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.carlos.movies_app.ui.theme.MoviesappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoviesappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
