392cbb7b3cac3988a5f4ef88fc1902dc75d3e53d
Carlos Alves <carlosaalves07@outlook.com>
2023-12-19 21:05:51 -0300

    feat: create utils.

---
+++ app/src/main/java/com/carlos/movies_app/util/Category.kt
@@ -0,0 +1,6 @@
+package com.carlos.movies_app.util
+
+object Category {
+    const val POPULAR = "popular"
+    const val UPCOMING = "upcoming"
+}
---
+++ app/src/main/java/com/carlos/movies_app/util/Screen.kt
@@ -0,0 +1,11 @@
+package com.carlos.movies_app.util
+
+sealed class Screen(
+    val rout: String
+) {
+    data object Home : Screen("main")
+    data object PopularMovieList : Screen("popularMovie")
+    data object UpcomingMovieList : Screen("upcomingMovie")
+    data object Details : Screen("detail")
+}
+
